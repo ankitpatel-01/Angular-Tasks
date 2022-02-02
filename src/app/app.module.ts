c6096c42c51cb393400b569f7680ee9dcd2c12bb
Ankitkumar Patel <ankitkumar.patel@1Rintern.local>
2022-02-02 11:59:12 +0530

    header and sidebar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
