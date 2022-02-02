9cd2b321d7394f23653d7bba7cc502b7a38bd064
Ankit Patel <ankitpatelak2017@gmail.com>
2022-02-02 22:52:14 +0530

    designed form and added validation name and email

--- src/app/modules/reactive-forms/profile-form/profile-form.component.ts
+++ src/app/modules/reactive-forms/profile-form/profile-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Department } from './department.model';
 
 @Component({
@@ -8,23 +9,45 @@ import { Department } from './department.model';
 })
 export class ProfileFormComponent implements OnInit {
 
-  departments:Department[] = [
+  departments: Department[] = [
     {
-      name: "FrontEnd"
+      "name": "UX"
     },
     {
-      name: "Database"
+      "name": "FrontEnd"
     },
     {
-      name: ".Net"
+      "name": "Database"
     },
     {
-      name: "QA"
+      "name": ".Net"
+    },
+    {
+      "name": "QA"
     }
-  ]
-  constructor() { }
+  ];
+
+  myReactiveForm: FormGroup;
+
+  constructor() {
+    this.myReactiveForm = new FormGroup(
+      {
+        "username": new FormControl('Ankitkumar Patel', [Validators.required, Validators.minLength(5)]),
+        "email": new FormControl('', [Validators.required, Validators.email]),
+        "contactNo": new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]),
+        "gender": new FormControl(null, Validators.required),
+        "department": new FormControl("FrontEnd", Validators.required),
+        "termsAccept": new FormControl(null, Validators.required)
+      }
+    );
+  }
 
   ngOnInit(): void {
+
+  }
+
+  onFormSubmit(): void {
+    console.log(this.myReactiveForm)
   }
 
 }
